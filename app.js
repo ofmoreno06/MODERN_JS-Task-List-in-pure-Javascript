69a75e6ee389c384b2ce000094eb5794901fd51f
Oscar Moreno <ofmoreno06@gmail.com>
2018-03-25 10:29:46 -0500

    add Remove Task functionality

--- app.js
+++ app.js
@@ -17,7 +17,8 @@
 	}
 
 	// ======================================
-	// addTask button
+	// addTask functionality
+
 	taskForm['add-task'].addEventListener('click', addTask);
 
 	function addTask(e){
@@ -28,8 +29,27 @@
 
 	function createTaskElement(task){
 		const li = document.createElement('li');
+		li.classList.add('list-group-item');
 		li.appendChild(document.createTextNode(task));
+		
+		const closeBtn = li.appendChild(document.createElement('a'));
+		closeBtn.classList.add('remove-task');
+		closeBtn.classList.add('float-sm-right');
+		closeBtn.innerHTML = '<i class="fas fa-times" style="cursor: pointer"></i>';	
+
 		return li;
 	}
+
+	// ======================================
+	// remove task functionality:
+	// - use 'Event Delegation', listening all events inside list and take action only when close icon is clicked.
+	
+	list.addEventListener('click', e => {
+		if (e.target.parentElement.classList.contains('remove-task'))
+			e.target.parentElement.parentElement.remove();
+
+		if (e.target.parentElement.parentElement.classList.contains('remove-task'))
+			e.target.parentElement.parentElement.parentElement.remove();
+	});
 
 })();
